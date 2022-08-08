2535f535492d6eab2375b74214856d635414ae3d
Aman <raiaman21@gmail.com>
2022-08-08 20:44:15 +0530

    UC2 Solved

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -1,16 +1,40 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class EmployeePayroll{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ID : ");
-        int id = sc.nextInt();
-        System.out.print("Enter name : ");
-        String name = sc.next();
-        System.out.print("Enter Salary : ");
-        int salary = sc.nextInt();
+        public static void main(String[] args) {
+            // Check File Exists
+        Path filePath = Paths.get("C://Users/raiam/Downloads/property.png");
 
-        System.out.println(" ");
-        System.out.println("Employee Info --> \n"+ "ID : "+ id + " Name : "+ name + " Salary : "+salary);
+        if(Files.exists(filePath)){
+            System.out.println("File is present");
+        }
 
+            // Delete file
+        if(Files.exists(filePath)){
+            try{
+                Files.delete(filePath);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+            // Create Directory
+        Path path = Paths.get("C://Users/raiam/Downloads/temp/");
+        try {
+            Files.createDirectory(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            // Create File
+        File myfile = new File("javafile.txt");
+            try{
+                myfile.createNewFile();
+            }
+            catch(Exception e){
+                System.out.println("Unable to create a file");
+            }
+        }
     }
-}
