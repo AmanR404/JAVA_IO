8d45c0fc4f2e9327c195c7a1ffb98c1329987a4f
Aman <raiaman21@gmail.com>
2022-08-08 21:42:20 +0530

    UC4 Solved

--- EmployeePayroll1.java
+++ EmployeePayroll1.java
@@ -3,27 +3,8 @@ import java.io.FileWriter;
 import java.util.Scanner;
 public class EmployeePayroll1{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ID : ");
-        String id = sc.next();
-        System.out.print("Enter name : ");
-        String name = sc.next();
-        System.out.print("Enter Salary : ");
-        String salary = sc.next();
-
-       try{
-        FileWriter fileWriter = new FileWriter("data.txt");
-        fileWriter.write("Employee Payroll data --> ");
-        fileWriter.write("ID : " + id);
-        fileWriter.write(" Name : " + name);
-        fileWriter.write(" Salary : " + salary);
-        fileWriter.close();
-       }
-       catch(Exception e){
-        System.out.println(e);
-       }
-
-       File file = new File("data.txt");
+      
+       File file = new File("employeePayrollFile.txt");
        try{
             Scanner scan = new Scanner(file);
             while(scan.hasNextLine()){
